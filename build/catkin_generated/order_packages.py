4cca49883862276aec03d369a172ed47b07970ba
DhilanIM <cls_dhilan@hotmail.com>
2021-09-24 11:07:52 -0500

    1st

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/dhilan/oruga_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/dhilan/ydlidar_ws/devel;/home/dhilan/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/dhilan/ydlidar_ws/devel;/home/dhilan/catkin_ws/devel;/opt/ros/melodic' != '' else []
